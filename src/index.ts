679f6f25d2ea233c2fbe45ec8acfa80815863fe0
Elliot Berman <berman5@purdue.edu>
2018-09-28 20:00:41 -0400

    Add interactive group

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './conductor';
 export * from './pros';
 export * from './types';
 export * from './v5';
+export * from './interactive';
---
+++ src/interactive.ts
@@ -0,0 +1,12 @@
+import { CLIEmitter, cliHook } from './util';
+import { Callbacks } from './types';
+
+export const createNewProjectInteractive = async (callbacks: Callbacks): Promise<number> => {
+  return cliHook(
+    new CLIEmitter(
+      'prosv5', [
+        'interactive', 'new_project'
+      ].filter(e => e !== '')
+    ), callbacks
+  );
+};
