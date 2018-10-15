91e1d3a49a9baa5f65400587a5e83e80cafb9b3b
Elliot Berman <elliot@elliotjb.com>
2018-10-15 15:45:59 -0400

    Add support for collapsed attribute in Container

--- src/input/interactive/components/Container.ts
+++ src/input/interactive/components/Container.ts
@@ -3,18 +3,39 @@ import { Component, ComponentArguments } from './Component';
 export type ContainerArguments = ComponentArguments & {
   title?: string,
   description?: string,
+  uuid: string,
+  collapsed: boolean,
   elements: ComponentArguments[]
 };
 
 export class Container extends Component {
   title?: string;
   description?: string;
+  uuid: string;
+  collapsed: boolean;
   elements: Component[];
 
-  constructor({title, description, elements, __componentFactory}: ContainerArguments) {
+  update_callback: (d: any) => void;
+
+  constructor({title, description, uuid, collapsed, elements, _update, __componentFactory}: ContainerArguments) {
     super();
     this.title = title;
     this.description = description;
+    this.uuid = uuid;
+    this.collapsed = collapsed;
+
     this.elements = elements.map(e => __componentFactory.createInstance(e));
+
+    this.update_callback = _update;
+  }
+
+  updateCollapsed(collapsed: boolean) {
+    this.update_callback({
+      event: 'update',
+      uuid: this.uuid,
+      args: [collapsed]
+    });
+
+    this.collapsed = collapsed;
   }
 }
