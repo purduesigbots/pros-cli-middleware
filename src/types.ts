5755c4e6b09add98737ea43c00f1472995b6bd71
Elliot Berman <berman5@purdue.edu>
2018-11-05 15:03:01 -0500

    Version check createNewProjectInteractive

--- src/types.ts
+++ src/types.ts
@@ -28,4 +28,6 @@ export type UploadProjectOptions = Partial<{run: boolean, name: string, slot: nu
 
 export type V5RemoveFileOptions = Partial<{all: boolean}>;
 
+export class UnsupportedVersionException extends Error {}
+
 export * from './input/interactive';
