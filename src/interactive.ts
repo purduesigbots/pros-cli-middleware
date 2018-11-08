dc76a7fb9956a53720e193209c982181a0c43155
Elliot Berman <berman5@purdue.edu>
2018-11-07 20:07:06 -0500

    Add updateProjectInteractive

--- src/interactive.ts
+++ src/interactive.ts
@@ -16,3 +16,18 @@ export const createNewProjectInteractive = async (callbacks: Callbacks): Promise
     return Promise.reject(new UnsupportedVersionException(`prosv5 interactive new_project requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
   }
 };
+
+export const updateProjectInteractive = async (callbacks: Callbacks): Promise<number> => {
+  const cliVersion = await getVersion();
+  if (gt(cliVersion, '3.1.2')) { // TODO: Make this the right version comparison
+    return cliHook(
+      new CLIEmitter(
+        'prosv5', [
+          'interactive', 'update_project'
+        ].filter(e => e !== '')
+      ), callbacks
+    );
+  } else {
+    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive update_project requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
+  }
+};
