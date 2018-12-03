8b3b72ca312472cbb2b69d2b1045077f276ae5e6
Elliot Berman <berman5@purdue.edu>
2018-12-03 15:55:13 -0500

    Expose uploadInteractive

--- src/interactive.ts
+++ src/interactive.ts
@@ -31,3 +31,18 @@ export const updateProjectInteractive = async (callbacks: Callbacks): Promise<nu
     return Promise.reject(new UnsupportedVersionException(`prosv5 interactive update_project requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
   }
 };
+
+export const uploadInteractive = async (callbacks: Callbacks): Promise<number> => {
+  const cliVersion = await getVersion();
+  if (gt(cliVersion, '3.1.2')) { // TODO: Make this the right version comparison
+    return cliHook(
+      new CLIEmitter(
+        'prosv5', [
+          'interactive', 'upload'
+        ].filter(e => e !== '')
+      ), callbacks
+    );
+  } else {
+    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive upload requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
+  }
+};
