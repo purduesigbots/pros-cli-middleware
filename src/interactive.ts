014f65125a554267c659c436506556739ba053d4
Elliot Berman <berman5@purdue.edu>
2019-01-22 10:09:23 -0500

    Update versions

--- src/interactive.ts
+++ src/interactive.ts
@@ -4,7 +4,7 @@ import { gt } from 'semver';
 
 export const createNewProjectInteractive = async (callbacks: Callbacks): Promise<number> => {
   const cliVersion = await getVersion();
-  if (gt(cliVersion, '3.1.2')) { // TODO: Make this the right version comparison
+  if (gt(cliVersion, '3.1.3')) { // TODO: Make this the right version comparison
     return cliHook(
       new CLIEmitter(
         'prosv5', [
@@ -13,13 +13,13 @@ export const createNewProjectInteractive = async (callbacks: Callbacks): Promise
       ), callbacks
     );
   } else {
-    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive new_project requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
+    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive new_project requires version > 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
   }
 };
 
 export const updateProjectInteractive = async (callbacks: Callbacks, { project }: UpdateProjectInteractiveOptions = {}): Promise<number> => {
   const cliVersion = await getVersion();
-  if (gt(cliVersion, '3.1.2')) { // TODO: Make this the right version comparison
+  if (gt(cliVersion, '3.1.3')) { // TODO: Make this the right version comparison
     const projectStr = project ? `--project "${project}"` : '';
     return cliHook(
       new CLIEmitter(
@@ -29,13 +29,13 @@ export const updateProjectInteractive = async (callbacks: Callbacks, { project }
       ), callbacks
     );
   } else {
-    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive update_project requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
+    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive update_project requires version > 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
   }
 };
 
 export const uploadInteractive = async (callbacks: Callbacks, { project }: UploadInteractiveOptions = {}): Promise<number> => {
   const cliVersion = await getVersion();
-  if (gt(cliVersion, '3.1.2')) { // TODO: Make this the right version comparison
+  if (gt(cliVersion, '3.1.3')) { // TODO: Make this the right version comparison
     const projectStr = project ? `--project "${project}"` : '';
     return cliHook(
       new CLIEmitter(
@@ -45,6 +45,6 @@ export const uploadInteractive = async (callbacks: Callbacks, { project }: Uploa
       ), callbacks
     );
   } else {
-    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive upload requires version >= 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
+    return Promise.reject(new UnsupportedVersionException(`prosv5 interactive upload requires version > 3.1.3. You have ${cliVersion}. Update CLI or downgrade plugin`));
   }
 };
